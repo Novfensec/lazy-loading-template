011453598c9cf87eef2501c7445491ccc5f0d6ea
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2024-12-30 14:50:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import webbrowser
 
 from kivymd.app import MDApp
-from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.transition import MDSharedAxisTransition as SAT
 from kivymd.utils.set_bars_colors import set_bars_colors
 from kivy.core.window import Window
